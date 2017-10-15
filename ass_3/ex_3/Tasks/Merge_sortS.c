530ffb16161a3a7a59c6dd32fc23eaa5c6b94b9b
Tim Svensson <svensson_tim@hotmail.se>
2017-10-15 15:49:08 +0200

    Bullshit push it

--- ass_3/ex_3/Tasks/Merge_sortS.c
+++ ass_3/ex_3/Tasks/Merge_sortS.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <omp.h>
 #include "timer.h"
+#include <math.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define TASK_LIMIT 16 //sysconf(_SC_NPROCESSORS_ONLN)
 
 void merge(int a[], int temp[], int low, int high, int mid) {
   int i, j, k;
@@ -24,14 +30,21 @@ void merge(int a[], int temp[], int low, int high, int mid) {
   }
 }
 
-void mergesort(int a[], int temp[], int low, int high) {
+void mergesort(int a[], int temp[], int low, int high, int num_tasks) {
   int mid;
-  if (low < high) {
-    mid = (low+high)/2;              // find the midpoint
-    mergesort(a, temp, low, mid);    // sort the first half
-    mergesort(a, temp, mid+1, high); // sort the second half
-    merge(a, temp, low, high, mid);  // merge them together into one sorted list
-  }
+  if (low < high)
+    {
+      mid = (low+high)/2;              // find the midpoint
+      
+      bool do_task = ((int) pow(2, (double)num_tasks)) <= TASK_LIMIT;
+      //      printf("Tasks: %d, Do: %d\n", num_tasks, do_task);
+      
+#pragma omp task if( do_task )
+      mergesort(a, temp, low, mid, ++num_tasks);    // sort the first half
+      mergesort(a, temp, mid+1, high, ++num_tasks); // sort the second half
+#pragma omp taskwait
+      merge(a, temp, low, high, mid);  // merge them together into one sorted list
+    }
 }
 
 int main(int argc, char **argv) {
@@ -58,10 +71,12 @@ int main(int argc, char **argv) {
   for (i = 0; i < LEN; i++) printf("%d ", x[i]);
   printf("\n");
 #endif
-  
+
+  //  printf("Task limit: %ld\n", TASK_LIMIT);
+
   initialize_timer ();  //We are just timing the sort
   start_timer();
-  mergesort(x, temp, 0, (LEN-1));
+  mergesort(x, temp, 0, (LEN-1), 0);
   stop_timer();
   time = elapsed_time();
   
