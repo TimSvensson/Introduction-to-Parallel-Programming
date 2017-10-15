84104b405e7ab4c60233d3ff5449ab8d9f262c4b
Tim Svensson <svensson_tim@hotmail.se>
2017-10-15 13:45:51 +0200

    as3 ex3, first commit.

---
+++ ass_3/ex_3/Tasks/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+r_file=results.txt
+echo "" > $r_file
+
+./Merge_sort >> $r_file
+./Merge_sort >> $r_file
+./Merge_sort >> $r_file
+./Merge_sort >> $r_file
+./Merge_sort >> $r_file
+
+./Merge_sort >> $r_file
+./Merge_sort >> $r_file
+./Merge_sort >> $r_file
+./Merge_sort >> $r_file
+./Merge_sort >> $r_file
