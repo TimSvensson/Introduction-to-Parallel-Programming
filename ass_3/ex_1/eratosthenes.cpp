0ef593fbba6b6a6e47bfc02921b1642b676deb0d
zohrath <zohrath@gmail.com>
2017-10-17 13:40:59 +0200

    erastothenes parallel

--- ass_3/ex_1/eratosthenes.cpp
+++ ass_3/ex_1/eratosthenes.cpp
@@ -124,7 +124,7 @@ int main(int argc, const char * argv[]) {
         auto finish = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> elapsed = finish - start;
         std::cout << "Elapsed time: " << elapsed.count() << " s\n";
-        printList(result, "");
+        //printList(result, "");
     }
     
     return 0;
