e12164309a5abaab6c893546c3a221c7496a6491
Adam Woods <adwo3009@gullviva.it.uu.se>
2017-10-23 15:36:44 +0200

    kukskaft

--- ass_4/HelloWorld.c
+++ ass_4/HelloWorld.c
@@ -3,10 +3,17 @@
 
 int main(int argc, char *argv[])
 {
-    int rank, size;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    printf("I am %d of %d\n", rank, size);
-    MPI_Finalize();
-    return 0; }
+    	int rank, size;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	printf("I am %d of %d\n", rank, size);
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
+	int name_len;
+	MPI_Get_processor_name(processor_name, &name_len);
+	printf("Hello world from processor %s, rank %d" " out of %d processors\n", processor_name, rank, size);
+	MPI_Finalize();
+	
+	return 0; 
+
+}
