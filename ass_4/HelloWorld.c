635d1376095b489ad408c804635a2ed51a9f3611
Adam Woods <zohrath@gmail.com>
2017-10-27 10:57:05 +0200

    YAOW

--- ass_4/HelloWorld.c
+++ ass_4/HelloWorld.c
@@ -7,13 +7,15 @@ int main(int argc, char *argv[])
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	printf("I am %d of %d\n", rank, size);
+		
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
-	printf("Hello world from processor %s, rank %d" " out of %d processors\n", processor_name, rank, size);
+    	int name_len;
+   	MPI_Get_processor_name(processor_name, &name_len);
+
+        //Print off a hello world message
+    	printf("(%s), rank %d out of %d processors\n", processor_name, rank, size);
+
 	MPI_Finalize();
-	
 	return 0; 
 
 }
