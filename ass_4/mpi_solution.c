7b056da35da8e89d01d9b1729c13bbbfbec203da
Adam Woods <zohrath@gmail.com>
2017-10-27 11:03:24 +0200

    kuksugare

--- ass_4/mpi_solution.c
+++ ass_4/mpi_solution.c
@@ -48,3 +48,54 @@ int main(int argc, char** argv)
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
 }
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    if (argc == 1) {
+        cout << "Usage: ./main 'number'.\n";
+    }else {
+        
+        int threadCount = thread::hardware_concurrency();
+        //int chunks = threadCount/2;
+        int max = stoi(argv[1]);
+        int squareOfMax = ceil(sqrt(max)) + 1;
+        
+        
+        vector<int> listUnfiltered(max), listRemaining(max - squareOfMax);
+        iota(listUnfiltered.begin(), listUnfiltered.end(), 1);
+        
+        // Loops through a list of integers and sets any non-prime to zero
+        for (int k = 2; k*k <= squareOfMax; k++) {
+            for (int i = k*k; i <= squareOfMax; i++) {
+                if (listUnfiltered[i - 1] % k == 0  )  {
+                    listUnfiltered[i - 1 ] = 0;
+                }
+            }
+            cout << "Finished checking for multiples of " << k << "\n";
+        }
+        
+        // Filter out zeroes from above list, use this list to compare the other lists
+        vector<int> primeMultiples = filterList(listUnfiltered, squareOfMax);
+        printList(primeMultiples, "Filtered list: ");
+        
+        // Fill a list of remaining ints starting from +1 of the filtered list's last number
+        iota(listRemaining.begin(), listRemaining.end(), (primeMultiples.back() + 1));
+        
+        // Checks the remaining list for multiples of the filtered primes and sets these to zero
+        for (int i = 0; i < listRemaining.size(); i++) {
+            for (int y = 0; y < primeMultiples.size(); y++) {
+                if (listRemaining[i] % primeMultiples[y] == 0) {
+                    listRemaining[i] = 0;
+                }
+            }
+        }
+        
+        
+        divideList(max, squareOfMax, threadCount, primeMultiples);
+        
+        
+    }
+    
+    return 0;
+}
